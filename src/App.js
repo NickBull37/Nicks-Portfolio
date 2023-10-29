b45b50a169030dfff8e9e3d683e18d32f222c9c2
Nick <nbulisky@gmail.com>
2023-10-29 10:45:40 -0400

    Removed unused dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from "./components";
 
 function App() {
   return (
 
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" exact element={<HomePage />} />
-        </Routes>
-    </BrowserRouter>
+    <HomePage />
 
   );
 }
